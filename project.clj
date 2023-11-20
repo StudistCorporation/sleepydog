fcaf880a2f082fed18941fa0f15a038989599e19
Erdos Balint <valerauko@gmail.com>
2023-11-20 16:18:27 +0900

    Use kaocha for running tests

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
                                    :username :env/clojars_user
                                    :password :env/clojars_token}}
   :profiles
-  {:dev {:dependencies [[clj-kondo "2023.10.20"]]
+  {:dev {:dependencies [[clj-kondo "2023.10.20"]
+                        [lambdaisland/kaocha "1.85.1342"]]
          :aliases {"lint" ["run" "-m" "clj-kondo.main"
                            "--config" ".clj-kondo/config.edn"
                            "--lint" "src" "test"]}}})
