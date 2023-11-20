fe10266fedb3076f00df71809ec39d251dcee08d
Erdos Balint <valerauko@gmail.com>
2023-11-20 15:12:33 +0900

    Add missing dependency

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "https://github.com/StudistCorporation/sleepydog"
   :license {:name "MIT"
             :url "https://opensource.org/license/mit/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [com.datadoghq/dd-trace-ot "1.17.0"]]
   :deploy-repositories {"clojars" {:url "https://repo.clojars.org/"
                                    :username :env/clojars_user
                                    :password :env/clojars_token}}
