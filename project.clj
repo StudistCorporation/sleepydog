951a69a2620e11036e4238eb721710ec2fd26413
Erdos Balint <valerauko@gmail.com>
2023-11-20 17:58:09 +0900

    Bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jp.studist/sleepydog "0.1.0-alpha3"
+(defproject jp.studist/sleepydog "0.1.0-alpha4"
   :description "FIXME: write description"
   :url "https://github.com/StudistCorporation/sleepydog"
   :license {:name "MIT"
