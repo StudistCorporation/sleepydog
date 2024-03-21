417f50d830f84a26c384974f6702477df35b7c80
Erdos Balint <valerauko@gmail.com>
2024-03-21 15:47:31 +0900

    Add test case

--- test/sleepy/dog/carmine_test.clj
+++ test/sleepy/dog/carmine_test.clj
@@ -6,4 +6,8 @@
 (deftest trace-carmine-test
   (testing "carmine vars are instrumented"
     (carmine-dog/trace-carmine!)
-    (is (true? (:sleepy.dog/traced? (meta carmine/get))))))
+    (is (true? (:sleepy.dog/traced? (meta carmine/get)))))
+  (testing "The instrumentation call is idempotent"
+    (carmine-dog/trace-carmine!)
+    (is (true? (:sleepy.dog/traced? (meta carmine/get))))
+    (is (fn? carmine/get))))
